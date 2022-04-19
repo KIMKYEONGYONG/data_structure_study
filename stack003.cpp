a5b56fb5e9be277b4bc65d5b8154c74182124b7e
KIMKYEONGYONG <rlaruddyd1@naver.com>
2022-04-19 10:19:05 +0900

    모듈화

--- stack003.cpp
+++ stack003.cpp
@@ -5,17 +5,20 @@ using std::cout; using std::endl;
 using std::string; using std::cin;
 using std::equal; using std::remove;
 
+string checkPalindrome(string& s) {
+    string tmp = s;
+
+    if (equal(tmp.begin(), tmp.begin() + tmp.size()/2, tmp.rbegin())) {
+        return s + " is a palindrome";
+    } else {
+        return s + " is not a palindrome";
+    }
+}
+
 int main(){
     string s1 = "radar";
     string s2 = "Was it a cat I saw";
-
-    equal(s1.begin(), s1.begin() + s1.size()/2, s1.rbegin()) ?
-        cout << "s1 is a palindrome" << endl :
-        cout << "s1 is not a palindrome" << endl;
-
-    equal(s2.begin(), s2.begin() + s2.size()/2, s2.rbegin()) ?
-        cout << "s2 is a palindrome" << endl :
-        cout << "s2 is not a palindrome" << endl;
-
+    cout << checkPalindrome(s1)<< endl;
+	  cout << checkPalindrome(s2)<< endl;
     return EXIT_SUCCESS;
 }
